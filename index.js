93d4f686a84b4243ec152ff8398db442f2df5ce6
Logan Van Hook <loganvan@att.net>
2020-03-17 14:55:36 -0400

    MVP

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const server = express();
 
+server.use(express.json());
+
+const DB = require("./data/mainDB");
+
 server.get("/", (req, res)=>
 {
-    res.status(200).send("ok");
+    DB("cars").select("*").then((response)=>
+    {
+        res.status(200).json(response);
+    }).catch((error)=>
+    {
+        res.status(500).send("Internal server error");
+    });
 });
+
+server.post("/", (req, res)=>
+{
+    DB("Cars").insert(req.body).then((response)=>
+    {
+        res.status(200).json(response);
+    }).catch((error)=>
+    {
+        res.status(500).send(error);
+    })
+})
 
 server.listen(5000);
